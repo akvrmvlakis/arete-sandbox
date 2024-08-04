d76befeaa2d415ca63b11f78348a490e0e4ba2b3
Alexandros Kourmoulakis <pro_balla2000@hotmail.com>
2024-08-04 12:39:35 +0300

    modify route.ts and test with minimal setup to isolate the issue

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,33 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import nodemailer from "nodemailer";
 
 export async function POST(request: NextRequest) {
-  try {
-    const { email, name, message } = await request.json();
-
-    // Create a transporter using environment variables
-    const transport = nodemailer.createTransport({
-      host: process.env.SMTP_HOST as string,
-      port: Number(process.env.SMTP_PORT),
-      auth: {
-        user: process.env.SMTP_USER as string,
-        pass: process.env.SMTP_PASS as string,
-      },
-    });
-
-    // Define mail options
-    const mailOptions = {
-      from: "akvrmvlakis@gmail.com", // Use the visitor's email address as the sender
-      to: "pro_balla2000@hotmail.com",
-      subject: `Message from ${name} (${email})`,
-      text: message,
-    };
-
-    // Send the email
-    await transport.sendMail(mailOptions);
-    return NextResponse.json({ message: "Email sent successfully" });
-  } catch (err: unknown) {
-    const errorMessage = (err as Error).message || "An unknown error occurred";
-    return NextResponse.json({ error: errorMessage }, { status: 500 });
-  }
+  return NextResponse.json({ message: "POST request received" });
 }
