ff3270a0d898280b3997007c0dcfb6da7ccc0005
Alexandros Kourmoulakis <pro_balla2000@hotmail.com>
2024-08-04 23:49:10 +0300

    change fetch path to absolute path on send-email.ts

--- src/app/utils/send-email.ts
+++ src/app/utils/send-email.ts
@@ -2,7 +2,7 @@ import { FormData } from "@/components/Form";
 
 // src/app/utils/send-email.ts
 export async function sendEmail(data: FormData) {
-  const response = await fetch("/api/email", {
+  const response = await fetch("https://arete-sandbox.vercel.app/api/email", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
