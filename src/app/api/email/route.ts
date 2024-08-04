7cced7b077a4d57c79f454253f25967a0f511689
Alexandros Kourmoulakis <pro_balla2000@hotmail.com>
2024-08-04 12:31:06 +0300

    modify route.ts

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,30 +1,32 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
 export async function POST(request: NextRequest) {
-  const { email, name, message } = await request.json();
-
-  const transport = nodemailer.createTransport({
-    host: process.env.SMTP_HOST as string,
-    port: Number(process.env.SMTP_PORT),
-    auth: {
-      user: process.env.SMTP_USER as string,
-      pass: process.env.SMTP_PASS as string,
-    },
-  });
-
-  const mailOptions: nodemailer.SendMailOptions = {
-    from: "akvrmvlakis@gmail.com",
-    to: "pro_balla2000@hotmail.com",
-    subject: `Message from ${name} (${email})`,
-    text: message,
-  };
-
   try {
+    const { email, name, message } = await request.json();
+
+    // Create a transporter using environment variables
+    const transport = nodemailer.createTransport({
+      host: process.env.SMTP_HOST as string,
+      port: Number(process.env.SMTP_PORT),
+      auth: {
+        user: process.env.SMTP_USER as string,
+        pass: process.env.SMTP_PASS as string,
+      },
+    });
+
+    // Define mail options
+    const mailOptions = {
+      from: "akvrmvlakis@gmail.com", // Use the visitor's email address as the sender
+      to: "pro_balla2000@hotmail.com",
+      subject: `Message from ${name} (${email})`,
+      text: message,
+    };
+
+    // Send the email
     await transport.sendMail(mailOptions);
-    return NextResponse.json({ message: "Email sent" });
+    return NextResponse.json({ message: "Email sent successfully" });
   } catch (err: unknown) {
-    // Type assertion to `Error`
     const errorMessage = (err as Error).message || "An unknown error occurred";
     return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
